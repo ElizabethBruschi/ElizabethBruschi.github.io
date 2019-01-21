b39a244e41f8b12197ee4453d93c1272c09b7dc9
Elizabeth Bruschi <fireflygirl11@gmail.com>
2019-01-21 13:11:36 -0700

    fgdkjljla

---
+++ script.js
@@ -0,0 +1,19 @@
+function changeslid(sp) {
+    var s;
+    var slid = document.getElementsByClassName("slidspic");
+    var indicator = document.getElementsByClassName("ind")
+    if (sp > slid.length) { slidpos = 1 };
+    if (sp < 1) { slidpos = slid.length };
+    for (s = 0; s < slid.length; s++) {
+        slid[s].style.display = "none";
+        indicator[s].style.backgroundColor = "white";
+    }
+    slid[slidpos - 1].style.display = "block";
+    indicator[slidpos - 1].style.backgroundColor = "black";
+}
+function change(n) {
+    changeslid(slidpos += n);
+}
+if (document.getElementsByClassName("slidspic").length > 0) {
+    changeslid(slidpos);
+}
